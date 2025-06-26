e72c3fa06f8385ad9fc5642f0cd672f3ef267ae5
Andx123 <103997274+Andx123@users.noreply.github.com>
2025-06-26 16:14:14 +0200

    Update app.py with error handling

--- app/app.py
+++ app/app.py
@@ -6,8 +6,14 @@ r = redis.Redis(host="redis", port=6379)
 
 @app.route("/")
 def hello():
-    r.incr("visits")
-    return f"Hello! You have visited this page {r.get('visits').decode()} times."
+    try:
+        r.incr("visits")
+        visits = r.get("visits").decode()
+    except redis.exceptions.RedisError as e:
+        app.logger.error(f"Redis error: {e})
+        visits = "unknown"
+    
+    return f"Hello! You have visited this page {visits} times."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

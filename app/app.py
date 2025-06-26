e260702d46ac18b836bf01d8db6a923738bc7e47
Andx123 <103997274+Andx123@users.noreply.github.com>
2025-06-26 16:41:17 +0200

    added comments in app.py

--- app/app.py
+++ app/app.py
@@ -1,15 +1,18 @@
+# This script starts a Flask (web) application connected to Redis to count page visits
 from flask import Flask
 import redis
 
 app = Flask(__name__)
-r = redis.Redis(host="redis", port=6379)
+r = redis.Redis(host="redis", port=6379)  # connect to Redis service from docker-compose, "redis" is the hostname of the Redis container in docker-compose.ymal
 
 @app.route("/")
 def hello():
     try:
+        # increment the visit count in Redis
         r.incr("visits")
         visits = r.get("visits").decode()
     except redis.exceptions.RedisError as e:
+        # log the error and display 'unkown' if Redis is unavailable
         app.logger.error(f"Redis error: {e}")
         visits = "unknown"  
     return f"Hello! You have visited this page {visits} times."
